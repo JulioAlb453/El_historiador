cc2b6353a2a15e19ea0c3221c05e8de8a9fd9754
JulioAlb453 <fulioibeto18@gmail.com>
2025-03-12 01:14:57 -0600

    add conn and send message to publiher API
    Se añadieron lo ssiguiente para poder enviar mensajes a la cola de RabbitMQ
    
    - conexion a RabbitMQ
    - consumidor del evento
    - mandar mensaje hacia la api publicadora
    
    Se realizaron estos cambios
    
    -en el main se realiza ahora la carga de variables de entorno, iniciar el enrutamiento. Función anónima que se encarga de consumir mensajes de una cola de RabbitMQ y procesarlos
    
    -enviar el payload en el controlador para crear una nueva noticia
    iniciar las dependencias

--- newspaper/infraestructure/controllers/create_news_handler.go
+++ newspaper/infraestructure/controllers/create_news_handler.go
@@ -1,52 +1,75 @@
 package controllers
 
 import (
-	"context"
-	"main/newspaper/application"
-	"main/newspaper/domain"
-	"net/http"
+    "context"
+    "log"
+    "main/newspaper/application"
+    "main/newspaper/domain"
+    "main/newspaper/infraestructure/service"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 type NewSaveController struct {
-	UseCase *application.CreateNewUseCase
+    UseCase *application.CreateNewUseCase
 }
 
 func NewsSaveController(useCase *application.CreateNewUseCase) *NewSaveController {
-	return &NewSaveController{UseCase: useCase}
+    return &NewSaveController{UseCase: useCase}
 }
 
 func (nc *NewSaveController) NewCreateNewHandler(c *gin.Context) {
-	var new domain.News
+    var new domain.News
 
-	if err := c.ShouldBindJSON(&new); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
-	}
+    if err := c.ShouldBindJSON(&new); err != nil {
+        c.JSON(400, gin.H{"error": err.Error()})
+        return
+    }
 
-	validations := []struct {
-		condition bool
-		err       error
-	}{
-		{new.Title == "", domain.ErrMissingTitle},
-		{new.Author == "", domain.ErrMissingAuthor},
-		{new.Content == "", domain.ErrMissingContent},
-		{new.Description == "", domain.ErrMissingDescription},
-		{new.PublicationDate == "", domain.ErrMissingPublicationDate},
-		{new.Topic == "", domain.ErrMissingTopic},
-	}
+    validations := []struct {
+        condition bool
+        err       error
+    }{
+        {new.Title == "", domain.ErrMissingTitle},
+        {new.Author == "", domain.ErrMissingAuthor},
+        {new.Content == "", domain.ErrMissingContent},
+        {new.Description == "", domain.ErrMissingDescription},
+        {new.PublicationDate == "", domain.ErrMissingPublicationDate},
+        {new.Topic == "", domain.ErrMissingTopic},
+    }
 
-	for _, v := range validations {
-		if v.condition {
-			c.JSON(400, gin.H{"error": v.err.Error()})
-			return
-		}
-	}
+    for _, v := range validations {
+        if v.condition {
+            c.JSON(400, gin.H{"error": v.err.Error()})
+            return
+        }
+    }
 
-	if err := nc.UseCase.Execute(context.Background(), new); err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
-	}
+    log.Println("Creando noticia:", new)
 
-	c.JSON(http.StatusOK, gin.H{"message": "Noticia creada exitosamente"})
+    if err := nc.UseCase.Execute(context.Background(), new); err != nil {
+        c.JSON(500, gin.H{"error": err.Error()})
+        return
+    }
 
+    payload := map[string]interface{}{
+        "title":             new.Title,
+        "content":           new.Content,
+        "author":            new.Author,
+        "description":       new.Description,
+        "publicationDate":   new.PublicationDate,
+        "topic":             new.Topic,
+    }
+
+    log.Println("Enviando payload al publisher:", payload)
+
+    err := service.SendEventToPublisher(payload)
+    if err != nil {
+        log.Printf("Error al enviar payload al publisher: %v", err)
+    } else {
+        log.Println("Payload enviado al publisher exitosamente")
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "Noticia creada exitosamente"})
 }
--- newspaper/infraestructure/dependecies.go
+++ newspaper/infraestructure/dependecies.go
@@ -1,49 +1,60 @@
 package infraestructure
 
 import (
-	"log"
-	"main/core"
-	"main/newspaper/application"
-	"main/newspaper/infraestructure/controllers"
-	"main/newspaper/infraestructure/repository"
+    "log"
+    "main/core"
+    "main/newspaper/application"
+    "main/newspaper/infraestructure/controllers"
+    "main/newspaper/infraestructure/repository"
+    "os"
+
+    "github.com/joho/godotenv"
 )
 
 type Dependencies struct {
-	NewsSaveController    *controllers.NewSaveController
-	GetAllNewsController  *controllers.NewNewsGetAllController
-	GetNewsByIdController *controllers.NewGetNewsByIdController
-	DeleteNewsController  *controllers.NewsDeleteController
-	UpdateNewsController *controllers.NewsUpdateController
+    NewsSaveController      *controllers.NewSaveController
+    GetAllNewsController    *controllers.NewNewsGetAllController
+    GetNewsByIdController   *controllers.NewGetNewsByIdController
+    DeleteNewsController    *controllers.NewsDeleteController
+    UpdateNewsController    *controllers.NewsUpdateController
+    ProcessNewsConsumer     *repository.NewsConsumer
 }
 
 func Init() *Dependencies {
-	conn := core.Connect()
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error al cargar el archivo .env: %v", err)
+    }
 
-	if conn == nil {
-		log.Fatal("Error al conectar la base de datos")
-	}
-	db := conn.Database("El_historiador")
+    conn := core.Connect()
+    if conn == nil {
+        log.Fatal("Error al conectar la base de datos")
+    }
+    db := conn.Database(os.Getenv("MONGODB_DATABASE"))
 
-	newsRepo := repository.NewMongoNewsRepository(db)
+    rabbitConn := core.ConnectRabbitMQ() 
+    newsRepo := repository.NewMongoNewsRepository(db)
 
-	createNewsUseCase := application.NewCreateNewsUseCase(newsRepo)
-	getAllNewsUseCase := application.NewGetAllNewsUseCase(newsRepo)
-	getNewsByIdUseCase := application.NewGetNewByIdUseCase(newsRepo)
-	deleteNewsUseCase := application.NewDeleteNewUseCase(newsRepo)
-	updateNewsUseCase := application.NewUpdateNewUseCase(newsRepo)
+    createNewsUseCase := application.NewCreateNewsUseCase(newsRepo)
+    getAllNewsUseCase := application.NewGetAllNewsUseCase(newsRepo)
+    getNewsByIdUseCase := application.NewGetNewByIdUseCase(newsRepo)
+    deleteNewsUseCase := application.NewDeleteNewUseCase(newsRepo)
+    updateNewsUseCase := application.NewUpdateNewUseCase(newsRepo)
 
-	NewsSaveController := controllers.NewsSaveController(createNewsUseCase)
-	getAllNewsController := controllers.GetAllNewsController(getAllNewsUseCase)
-	getNewsByIdController := controllers.NewNewsGetByIdController(getNewsByIdUseCase)
-	deleteNewsController := controllers.NewNewsDeleteController(deleteNewsUseCase)
-	updateNewsController := controllers.NewNewsUpdateController(updateNewsUseCase)
+    NewsSaveController := controllers.NewsSaveController(createNewsUseCase)
+    getAllNewsController := controllers.GetAllNewsController(getAllNewsUseCase)
+    getNewsByIdController := controllers.NewNewsGetByIdController(getNewsByIdUseCase)
+    deleteNewsController := controllers.NewNewsDeleteController(deleteNewsUseCase)
+    updateNewsController := controllers.NewNewsUpdateController(updateNewsUseCase)
 
+    newsConsumer := repository.NewNewsConsumer(rabbitConn)
 
-	return &Dependencies{
-		NewsSaveController:    NewsSaveController,
-        GetAllNewsController:  getAllNewsController,
-        GetNewsByIdController: getNewsByIdController,
-        DeleteNewsController:  deleteNewsController,
-		UpdateNewsController : updateNewsController,
-	}
+    return &Dependencies{
+        NewsSaveController:      NewsSaveController,
+        GetAllNewsController:    getAllNewsController,
+        GetNewsByIdController:   getNewsByIdController,
+        DeleteNewsController:    deleteNewsController,
+        UpdateNewsController:    updateNewsController,
+        ProcessNewsConsumer:     newsConsumer,
+    }
 }
